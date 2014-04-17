468bd240d7eaf4d73ef00180c3bd9df7714dc130
Damian Kowal <damian@damiankowal.com>
2014-04-17 08:43:17 -0400

    Add tiles to initial values to make pairs

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@ var app = app || {};
 
 $(function() {
     var tiles = [
-        { value: 'A' },
-        { value: 'B' },
-        { value: 'C' },
-        { value: 'D' },
-        { value: 'E' },
-        { value: 'F' },
-        { value: 'G' },
-        { value: 'H' }
+        { value: 'A' }, { value: 'A' },
+        { value: 'B' }, { value: 'B' },
+        { value: 'C' }, { value: 'C' },
+        { value: 'D' }, { value: 'D' },
+        { value: 'E' }, { value: 'E' },
+        { value: 'F' }, { value: 'F' },
+        { value: 'G' }, { value: 'G' },
+        { value: 'H' }, { value: 'H' }
     ];
 
     new app.BoardView( tiles );
