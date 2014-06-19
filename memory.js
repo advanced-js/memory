c41afa249eb820494920140d1b0182e6f35aa4d7
futurus <futurus@live.com>
2014-06-19 17:23:45 -0400

    v3
    still need to implement 'score'

--- memory.js
+++ memory.js
@@ -1,25 +1,177 @@
 // YOUR CODE GOES HERE
-$(function(){
-    var firstClk, secondClk;
 
-    $('td').on('click',function(){
-        if (firstClk === undefined){
-            firstClk = $(this);
-            firstClk.text(firstClk.data('value'));
-        } else {
-            if ((secondClk === undefined) && ($(this).data('id') !== firstClk.data('id'))) {
-                secondClk = $(this);
-                secondClk.text(secondClk.data('value'));
+// defind the Tile object
+var Tile = function(id, value) {
+	this.id = id;
+	this.value = value;
+	this.state = null;
+}
 
-                setTimeout(function(){
-                    if (firstClk.data('value') !== secondClk.data('value')){
-                        firstClk.text('X');
-                        secondClk.text('X');
-                    }
-                    firstClk = undefined;
-                    secondClk = undefined;
-                },1000);
-            }
-        }
-    });
+Tile.prototype.reveal = function() {
+		this.state = 'DISPLAYED'
+}
+	
+Tile.prototype.hide = function() {
+		this.state = 'HIDDEN'
+}
+	
+Tile.prototype.matched = function() {
+		this.state = 'MATCHED'
+}
+
+// end of Tile object definition
+
+// defind the Board object
+var Board = function(size) {
+	this.size = size;
+	this.tiles = [];
+	this.cTiles = [];
+}
+
+// symbols borrowed from https://github.com/zenahirsch/memory/
+Board.prototype.symbols = [
+		'&#9742;', '&#9792;', '&#9794;', '&#9733;', '&#9824;', '&#9827;', '&#9775;', '&#9822;',
+		'&#9730;', '&#10084;', '&#9986;', '&#9873;', '&#9773;', '&#65533;', '&#63819;', '&#63498;',
+		'&#63499;', '&#9829;', '&#9830;', '&#9834;', '&#10016;', '&#10003;', '&#10007;', '&#63491;',
+		'&#8540;', '&#8531;', '&#8859;', '&#9651;',	'&#1590;', '&#1334;', '&#936;', '&#9762;', 
+		'&#43742;',	'&#12354;'
+	];
+	
+Board.prototype.score = 0;
+
+Board.prototype.populate = function() {
+	// declare variables
+	var i,
+		id = 0,
+		numOfPairs = (this.size * this.size)/2,
+		randInd,
+		tempTile;
+	
+	// initialize tiles
+	for (i = 0; i < numOfPairs; i++){
+		var t1 = new Tile(id  , this.symbols[i]);	this.tiles.push(t1);
+		var t2 = new Tile(id+1, this.symbols[i]);	this.tiles.push(t2);
+		id += 2;
+	}
+	
+	// randomize the tiles so the game is more interesting
+	for (i = 0; i < this.tiles.length; i++) {
+		// pick a random index that's not the current index
+		do {
+			randInd = Math.floor(Math.random() * this.tiles.length);
+		} while (randInd == i);
+		
+		tempTile = this.tiles[i];
+		this.tiles[i] = this.tiles[randInd];
+		this.tiles[randInd] = tempTile;
+	}
+	
+	// reset tile id so players can't cheat (view source)
+	for (i = 0; i < this.tiles.length; i++) {
+		this.tiles[i].id = i; // well, technically id should be private
+		this.tiles[i].hide();
+	}
+}
+
+Board.prototype.checkWin = function() {
+	var i;
+	
+	for (i = 0; i < this.tiles.length; i++) {
+		if (this.tiles[i].state !== 'MATCHED') {
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+Board.prototype.goLive = function() {
+	var cBoard = this; // grab a hold of this
+	
+	$('.tile').click(function () {
+		var id = $(this).attr('id');
+		
+		// enforce that NOTHING happens while checking result
+		// if it's HIDDEN, reveal it
+		if (cBoard.cTiles.length < 2 && cBoard.tiles[id].state === 'HIDDEN') {
+			cBoard.tiles[id].reveal();
+			$(this).html(cBoard.tiles[id].value);
+			cBoard.cTiles.push(cBoard.tiles[id]);
+			//$('#app-message').html(cBoard.cTiles.toString());
+		}
+				
+		if (cBoard.cTiles.length === 2) {
+		// if cTiles (current tiles) contains 2 elements, compare them
+		
+			var Tiles = cBoard.cTiles;
+			if (Tiles[0].value === Tiles[1].value) {
+				cBoard.tiles[Tiles[0].id].matched();
+				$('#app-board').find('td#' + Tiles[0].id).css('background-color', 'lightgreen');
+				cBoard.tiles[Tiles[1].id].matched();
+				$('#app-board').find('td#' + Tiles[1].id).css('background-color', 'lightgreen');
+				
+				cBoard.cTiles = [];
+			} else {
+				$('#app-board').find('td#' + Tiles[0].id).css('background-color', 'red');
+				$('#app-board').find('td#' + Tiles[1].id).css('background-color', 'red');
+				setTimeout(function(){
+								$('#app-board').find('td#' + Tiles[0].id).html('');
+								$('#app-board').find('td#' + Tiles[0].id).css('background-color', 'lightgray');
+								$('#app-board').find('td#' + Tiles[1].id).html('');
+								$('#app-board').find('td#' + Tiles[1].id).css('background-color', 'lightgray');
+								
+								cBoard.cTiles = [];
+								
+								cBoard.tiles[Tiles[0].id].hide();
+								cBoard.tiles[Tiles[1].id].hide();
+								//$('#app-message').html('&nbsp;');
+							}, 750);
+			}
+		}
+
+		// check if whole board is revealed, this is fired up every time there's a click, is there a way to do this more efficiently? 
+		// maybe put this in checkWin with a condition?
+		if (cBoard.checkWin()) {
+			$('#app-message').html('Your score is: ' + cBoard.score + '%, you won!');
+		}
+
+	});
+}
+
+Board.prototype.draw = function() {
+	var tInd = 0;
+	var html = '';
+
+	// constructing row for gameboard
+	for (var i = 0; i < this.size; i++) {
+		html += '<tr>';
+		// constructing column in each row
+		for (var x = 0; x < this.size; x++) {
+			html += '<td class="tile" id="' + this.tiles[tInd].id + '">' +
+				   '</td>';
+			tInd += 1;
+		}
+		html += '</tr>';
+	}
+
+	// update table with html code
+	$('#app-board').html(html);
+
+	// Add the event listeners
+	this.goLive();
+}
+// end of Board object definition
+
+// actual app
+$(function() {	
+	$('#app-button').click(function() {
+		var size = $('#app-select').val();
+		var board = new Board(size);
+		
+		board.populate();
+		board.draw();
+		
+		$('#app-message').html('&nbsp;');
+	});
 });
+// end of file
