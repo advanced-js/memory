d761bd30587b87ad867cce434e40558619c604b2
tsjoberg <tomgsjoberg@gmail.com>
2014-10-09 13:29:11 -0400

    Bonus Features Added
    Generate Board, Reset Board, Styling changes

--- memory.js
+++ memory.js
@@ -1,38 +1,87 @@
-var first;
-var second;
+$( function() {
 
-function storeVal(val) {
-    if( !first ) {  
-        first = val;
-    } else {
-        second = val;
-    } 
-}
+    var first;
+    var second;
+    var $board = $('#board');
 
-function isMatch(){
-    if ( first === second ) {
-        return true;
+    function generateBoard(values){
+        var arr = [];
+        var uniqueValues = values / 2;
+        for (i=0; i < uniqueValues ; i++){
+            arr.push(i);
+            arr.push(i);
+        };
+        arr = _.shuffle(arr); //randomize values
+        for (i=0; i < values; i++){
+            $board.append("<div class='col-md-2 col-sm-4 col-xs-6 cell'>" + arr[i] + "</div>"); 
+        }
+        enableClick();
     }
-    return false;
-}
 
-function resetGrid(){
-    $('.clicked').removeClass('clicked');
-    first = null;
-    second = null;
-}
+    function storeVal(val) {
+        if( !first ) {  
+            first = val;
+        } else {
+            second = val;
+        } 
+    }
 
-$(".cell").click(function(e){
-    var $obj = $(this);
-    if( !$obj.hasClass('clicked') && !$obj.hasClass('match') ){
-        var val = $obj.html();
-        $obj.addClass('clicked');
-        storeVal(val);
-        if(first && second) {
-            if ( isMatch() ){
-                $('.clicked').addClass('match');
+    function isMatch(){
+        if ( first === second ) {
+            return true;
+        }
+        return false;
+    }
+
+    function resetGrid(){
+        $('.clicked').removeClass('clicked');
+        first = null;
+        second = null;
+        enableClick();
+    }
+
+    function clickHandler(event){
+        var $obj = $(this);
+        if( !$obj.hasClass('clicked') && !$obj.hasClass('match') ){
+            var val = $obj.html();
+            $obj.addClass('clicked');
+            storeVal(val);
+            if(first && second) {
+                disableClick();
+                if ( isMatch() ){
+                    $('.clicked').addClass('match');
+                    resetGrid(); //allow immediate next click
+                } else {
+                    //allow user to see mismatch for 1.5 seconds
+                    setTimeout(resetGrid, 1500); 
+                }
             }
-            resetGrid();
         }
     }
+
+    function enableClick(){
+        $board.on('click', '.cell', clickHandler);
+    }
+
+    function disableClick(){
+        $board.off('click', '.cell', clickHandler);
+    }
+    
+    $("#generateBtn").click(function(e){
+        var gridSize = $("#gridSizeInput").val();
+        if (gridSize && gridSize % 2 === 0){
+            generateBoard(gridSize);
+            $(this).prop("disabled",true);
+            $("#resetBtn").show();
+        } else {
+            alert('Please enter valid value!');
+        }
+    });
+
+    $("#resetBtn").click(function(e){
+        $board.empty();
+        $("#generateBtn").prop("disabled", false);
+        $(this).hide();
+    });
+
 });
