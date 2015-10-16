d512015219894d0e4eeac2e59297b1824257937c
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-10-15 21:10:54 -0400

    make matched items invisible and check if item is matching itself (still buggy)

--- memory.js
+++ memory.js
@@ -4,10 +4,13 @@ $(function() {
 	$('td').click(function(){
 		if (lastClicked === undefined){
 			// first click
-			lastClicked = $(this).text();
-		}else if (lastClicked === $(this).text()){
+			lastClicked = this;
+		}else if (this != lastClicked && $(lastClicked).text() === $(this).text()){		
 			// second click - match
 			console.log("match!");
+			$(lastClicked).css("visibility","hidden");
+			$(this).css("visibility", "hidden");
+
 		} else {
 			// second click - no match
 			console.log("no match!");
