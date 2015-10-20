094532813793a8577739a538a0adbe8fd1d5fe9e
jonCravotta <jcravotta@renttherunway.com>
2015-10-20 16:30:05 -0400

    machine switch

--- js/script.js
+++ js/script.js
@@ -31,21 +31,42 @@ function counter(num, tdId) {
   }
 }
 
+
 var Click = (function() {
   this.numClicks = 0;
 });
 
 var clickTd = new Click();
+
+var ClickArr = (function() {
+  this.arr = [];
+  console.log("This is the og arr: " + this.arr);
+});
+
+var clickToArr = new ClickArr();
+
 $("td").click(function(e) {
   clickTd.incrementClicks();
+  var _this = this;
+  var tdId = $(_this).attr('class');
+  clickToArr.pushArr(tdId);
   // pieces.addToArr();
-})
+});
 
 
 Click.prototype.incrementClicks = function() {
   this.numClicks++;
-  console.log("Object: " + this.numClicks);
+  console.log("There have been: " + this.numClicks + " clicks");
   if ((this.numClicks % 2) === 0) {
-    check.checkCurrent();
+    //check.checkCurrent();
+    console.log("EVEN NUM");
   }
+};
+
+ClickArr.prototype.pushArr = function(tdId) {
+  this.arr.push(tdId);
+  console.log("This is the elements name: " + tdId);
+  console.log("This is the array: " + this.arr);
+  console.log("This is the arr length: " + this.arr.length);
+  console.log("This is the last element: " + this.arr[this.arr.length - 1]);
 };
