234ca961712e64a7fcb15a41f223de10b9463f34
Alex Chaparro <alex@kargo.com>
2015-10-19 21:15:53 -0400

    complete

--- memory.js
+++ memory.js
@@ -5,7 +5,7 @@ $(function() {
 		if (lastClicked === undefined){
 			// first click
 			lastClicked = this;
-		}else if (this != lastClicked && $(lastClicked).text() === $(this).text()){		
+		} else if (this != lastClicked && $(lastClicked).text() === $(this).text()){
 			// second click - match
 			console.log("match!");
 			$(lastClicked).css("visibility","hidden");
