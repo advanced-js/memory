9c885e169741d3ef4b949a04a3e76d522f58ae3a
katelynistoll <katelynistoll@gmail.com>
2015-10-22 09:25:43 -0400

    Added images

--- memory.js
+++ memory.js
@@ -1,8 +1,7 @@
-var content_array = ['A','A','K','K','Q','Q','J','J','10','10','9','9','8','8','7','7','6','6','5','5','4','4','3','3'];
+var content = ['blk10','blk10','blkace','blkace','blkjack','blkjack','blkjoker','blkjoker','blkking','blkking','blkqueen','blkqueen','red10','red10','redace','redace','redjack','redjack','redjoker','redjoker','redking','redking','redqueen','redqueen'];
 var values = [];
 var card_ids = [];
-var cards_flipped = 0;
-
+var cardsFlipped = 0;
 
 //Fisher Yates Shuffle Modern Algorithm
 Array.prototype.cardShuffle = function() {
@@ -13,26 +12,37 @@ Array.prototype.cardShuffle = function() {
 		this[j] = this[i];
 		this [i] = temp;
 	}
-}
+};
 
-
+//function called when page is refreshed or 'New Game' button is pressed
 function newGame(){
-	cards_flipped = 0;
+	cardsFlipped = 0;
 	var output = '';
-	content_array.cardShuffle();
-	for(var i = 0; i < content_array.length; i++){
-		output += '<div id="card_'+i+'" onclick="flipcard(this,\''+content_array[i]+'\')"></div>';
+	content.cardShuffle();
+	for(var i = 0; i < content.length; i++){
+		output += '<div id="card_'+i+'" onclick="playGame(this,\''+content[i]+'\')"></div>';
 	}
 	document.getElementById('gameboard').innerHTML = output;
 }
 
+//incorrect match, flip back over
+function tryAgain(){
+	var card1 = document.getElementById(card_ids[0]);
+	var card2 = document.getElementById(card_ids[1]);
+	card1.style.background = 'url(cards/cardback.png) no-repeat';
+	card1.innerHTML = "";
+	card2.style.background = 'url(cards/cardback.png) no-repeat';
+	card2.innerHTML = "";
+	values = [];
+	card_ids = [];
+};
 
-function flipcard(card,val){
-	if(card.innerHTML == "" && values.length < 2){
-		card.style.background = '#FFF';
-		card.innerHTML = val;
 
-		if(values.length == 0){
+function playGame(card,val){
+	if(card.innerHTML === "" && values.length < 2){
+		card.style.background = 'url(cards/'+ val +'.png) no-repeat';
+
+		if(values.length === 0){
 			values.push(val);
 			card_ids.push(card.id);
 
@@ -41,28 +51,20 @@ function flipcard(card,val){
 			card_ids.push(card.id);
 
 			if(values[0] == values[1]){
-				cards_flipped += 2;
+				cardsFlipped += 2;
 
 				values = [];
 				card_ids = [];
 
-				if(cards_flipped == content_array.length){
-					alert("You win! Click 'New Game' to play again.");
-				}
-			} else {
-				function tryAgain(){
-					var card_1 = document.getElementById(card_ids[0]);
-					var card_2 = document.getElementById(card_ids[1]);
-					card_1.style.background = 'url(cards/cardback.png) no-repeat';
-					card_1.innerHTML = "";
-					card_2.style.background = 'url(cards/cardback.png) no-repeat';
-					card_2.innerHTML = "";
-					values = [];
-					card_ids = [];
-				}
+            	//checks if all matches are complete and game is over
+            	if(cardsFlipped == content.length){
+            		alert("You win! Click 'New Game' to play again!");
+            	}
+			} else { 
 				setTimeout(tryAgain, 1000);
 			}
 		}
 	}
 }
 
+//studied Adam Khoury's array programming tutorials
