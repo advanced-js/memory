19a922124b201c4017adddfb9cd52bf6f9987ddf
git-ebetts <ebettsmail@gmail.com>
2015-10-27 14:35:54 -0400

    first commit from pair work in class

---
+++ memory.js
@@ -0,0 +1,31 @@
+var revealed = null;
+
+function clicked() { 
+    $(this).addClass('visible'); 
+
+    if (revealed == null) {
+        // first click
+        revealed = this.innerHTML;  
+
+    } else {
+        // second click
+        var thisValue = this.innerHTML; 
+
+        if (thisValue == revealed) {
+            // change color to yellow and make unclickable
+            $('.visible').addClass('colorMatched'); 
+
+            // then start over
+            reveal = null; 
+            $('.visible').removeClass('visible'); 
+
+        } else { 
+            // start over
+            reveal = null; 
+       }
+   }
+
+    console.log(this.innerHTML); 
+}; 
+
+$('span').on('click', clicked);  
