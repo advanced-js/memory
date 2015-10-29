3e4b3f01bfebb73b67ae6d9f9436e0f642e547df
mariannedo <mariannepdo@gmail.com>
2015-10-29 01:15:26 -0400

    OOPing Memory game

--- memory.js
+++ memory.js
@@ -1,54 +1,115 @@
-var revealed = null;
+var Memory = { 
 
-// functions
+    revealedCardVal: null,
 
-function clicked() { 
-    if(!$(this).hasClass('visible') && !$(this).hasClass('disabled')) { 
-        // only fire if what we clicked on is not already visible or disabled!
-        $(this).addClass('visible'); 
+    init: function() { 
+        // create an array of all the numbers for a 6x6 board
+        var numArray = []; 
 
-        if (revealed === null) {
-            // first click
-            revealed = this.innerHTML; 
+        for(var i = 1; i <= 18; i++) { 
+            numArray.push(i); 
+            numArray.push(i); 
+        }
 
-        } else {
-            // second click
-            var thisValue = this.innerHTML; 
+        var shuffledNumbers = this.shuffleArray(numArray); 
 
-            // if there's a match!
-            if (thisValue == revealed) {
-                // change color to yellow and make unclickable
-                $('.visible').addClass('colorMatched'); 
+        this.fillGame(shuffledNumbers); 
 
-                setTimeout(function() { 
-                // remove fancy yellow color
-                    $('.colorMatched').addClass('disabled'); 
-                }, 500); 
+        this.addEventListeners(); 
 
-                // is the game over? 
-                if($('.colorMatched').length == $('td').length) { 
-                    $('#Message').addClass('show'); 
+    }, 
+
+    shuffleArray: function(array) { 
+        // for reference: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }, 
+
+    fillGame: function(shuffledNums) { 
+        var tableEls = ""; 
+
+        for(var i = 0; i < shuffledNums.length; i++) { 
+            if((i + 6) % 6 === 0) { 
+                tableEls += '<tr><td><span>' + shuffledNums[i] + '</span></td>'; 
+            } else if ((i + 1) % 6 === 0) { 
+                tableEls += '<td><span>' + shuffledNums[i] + '</span></td></tr>'; 
+            } else { 
+                tableEls += '<td><span>' + shuffledNums[i] + '</span></td>'; 
+            }
+        }
+
+        $('#MemoryGame tbody').append(tableEls);
+    }, 
+
+    addEventListeners: function() { 
+        $('span').on('click', function() { 
+            if(!$(this).hasClass('visible') && !$(this).hasClass('disabled')) { 
+                $(this).addClass('visible'); 
+
+                if(Memory.revealedCardVal === null) {
+                    Memory.firstClick($(this).html()); 
+                } else { 
+                    Memory.secondClick($(this).html());
                 }
-            } 
+            }
+        }); 
+
+        $('#Reset').on('click', function() { 
+            Memory.reset(); 
+        }); 
+    }, 
+
+    firstClick: function(revealedVal) { 
+        Memory.revealedCardVal = revealedVal; 
+    }, 
+
+    secondClick: function(secondCardVal) { 
+        Memory.compareVals(secondCardVal); 
+
+        setTimeout(function() { 
+            Memory.revealedCardVal = null; 
+            $('.visible').removeClass('visible'); 
+        }, 600); 
+    }, 
+
+    compareVals: function(secondCardVal) { 
+        if (secondCardVal === Memory.revealedCardVal) {
+            $('.visible').addClass('colorMatched'); 
 
             setTimeout(function() { 
-                // after half a second, start over again!
-                revealed = null; 
-                $('.visible').removeClass('visible'); 
+                $('.colorMatched').addClass('disabled'); 
             }, 500); 
+
+            Memory.isGameOver(); 
+        } 
+    }, 
+
+    isGameOver: function() { 
+        if($('.colorMatched').length == $('td').length) { 
+            $('#Message').addClass('show'); 
         }
+    }, 
+
+    reset: function() { 
+        $('#Message').removeClass('show'); 
+        $('#MemoryGame tbody').html(''); 
+        
+        var numArray = []; 
+
+        for(var i = 1; i <= 18; i++) { 
+            numArray.push(i); 
+            numArray.push(i); 
+        }
+
+        var shuffledNumbers = Memory.shuffleArray(numArray); 
+
+        Memory.fillGame(shuffledNumbers); 
     }
 }
 
-
-function reset() { 
-    $('span').removeClass('visible').removeClass('colorMatched').removeClass('disabled'); 
-    $('#Message').removeClass('show'); 
-}
-
-// event listeners
-
-$('span').on('click', clicked); 
-
-$('#Reset').on('click', reset); 
-
+Memory.init(); 
